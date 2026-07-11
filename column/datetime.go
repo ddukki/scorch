9a979b15b00d782cf0389b126b98bd3ea9988fc9
Dukwhan Kim <ddukki@gmail.com>
2026-07-10 21:38:59 -0500

    feat: add DateTime column type with proper wire format
    Base[time.Time] sends 24 bytes per row (sizeof time.Time) but DateTime
    is 4 bytes on the wire (uint32 Unix timestamp). The generic Base[T]
    DecodeColumn/EncodeColumn uses unsafe.Sizeof(T) which is wrong for
    time.Time. New DateTime column type stores []uint32 internally with
    Append(time.Time)/Row() time.Time conversion.

---
+++ column/datetime.go
@@ -0,0 +1,67 @@
+package column
+
+import (
+	"time"
+
+	"github.com/ClickHouse/ch-go/proto"
+)
+
+type DateTime struct {
+	name string
+	Data []uint32
+}
+
+func NewDateTime(name string) *DateTime {
+	return &DateTime{name: name}
+}
+
+func (c *DateTime) Name() string { return c.name }
+
+func (c *DateTime) Type() proto.ColumnType { return proto.ColumnTypeDateTime }
+
+func (c *DateTime) Len() int { return len(c.Data) }
+
+func (c *DateTime) Append(v time.Time) {
+	c.Data = append(c.Data, uint32(v.Unix()))
+}
+
+func (c *DateTime) AppendArr(vs []time.Time) {
+	for _, v := range vs {
+		c.Data = append(c.Data, uint32(v.Unix()))
+	}
+}
+
+func (c *DateTime) Row(i int) time.Time {
+	return time.Unix(int64(c.Data[i]), 0)
+}
+
+func (c *DateTime) DecodeColumn(r *proto.Reader, rows int) error {
+	if rows == 0 {
+		c.Data = c.Data[:0]
+		return nil
+	}
+	c.Data = make([]uint32, rows)
+	for i := range c.Data {
+		v, err := r.UInt32()
+		if err != nil {
+			return err
+		}
+		c.Data[i] = v
+	}
+	return nil
+}
+
+func (c *DateTime) EncodeColumn(b *proto.Buffer) error {
+	for _, v := range c.Data {
+		b.PutUInt32(v)
+	}
+	return nil
+}
+
+func (c *DateTime) WriteColumn(w *proto.Writer) {
+	w.ChainBuffer(func(b *proto.Buffer) {
+		for _, v := range c.Data {
+			b.PutUInt32(v)
+		}
+	})
+}
